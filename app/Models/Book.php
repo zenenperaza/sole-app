9cda942898d2101d9473d1011c20407c77a7466a
zenenperaza <peraza@outlook.com>
2023-11-30 22:11:54 -0400

    add relations

---
+++ app/Models/Book.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = 'books';
+    protected $fillable = ['title', 'subtitle', 'language', 'page', 'published', 'description', 'genre_id', 'publisher_id'];
+
+    public function genre(){
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function publisher(){
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function authors(){
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+    
+}
---
+++ app/Models/Note.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notes';
+    protected $fillable = ['description', 'writing_date', 'user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function noteable(){
+        return $this->morphTo();
+    }
+
+}
